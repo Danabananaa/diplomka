ac756c0aad99fb32b36927c967b10e1b5b22e321
aldiyar.gibadatov@gmail.com <student@ALEM-F4-L04-06.alem.school>
2023-03-28 19:18:51 +0600

    add cmd

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,4 +1,46 @@
 package main
 
+import (
+	"net/http"
+	"time"
+
+	"diplomka/internal/handlers"
+	"diplomka/internal/repository"
+	"diplomka/internal/service"
+	"diplomka/pkg/log"
+	"diplomka/pkg/sqlite"
+
+	"github.com/gorilla/mux"
+)
+
+const port = "8080"
+
 func main() {
+	db, err := sqlite.Connect("./db/data.db")
+	if err != nil {
+		log.Fatalln(db)
+	}
+
+	uRepo := repository.NewUserRepo(db)
+	sRepo := repository.NewSessionRepo(db)
+
+	authS := service.NewAuthService(uRepo, sRepo)
+
+	authH := handlers.NewAuthHandlers(authS)
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/login", authH.LogIn).Methods(http.MethodPost)
+
+	server := http.Server{
+		Addr:         ":" + port,
+		Handler:      r,
+		WriteTimeout: 5 * time.Second,
+		ReadTimeout:  5 * time.Second,
+	}
+
+	log.Printf("Starting listener on http://localhost:%s", port)
+
+	err = server.ListenAndServe()
+	log.Fatalf("Server error: %v", err)
 }
