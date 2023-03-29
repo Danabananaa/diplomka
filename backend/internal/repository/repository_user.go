1c8432f6d8f0be0f1fd1005395b161b254f87d0c
bbaktyke <baktykeriyev97@mail.ru>
2023-03-29 20:43:56 +0600

    updated repo

--- backend/internal/repository/repository_user.go
+++ backend/internal/repository/repository_user.go
@@ -2,7 +2,10 @@ package repository
 
 import (
 	"context"
+	"database/sql"
 	"diplomka/internal/model"
+	"errors"
+	"fmt"
 
 	"github.com/jmoiron/sqlx"
 )
@@ -20,16 +23,36 @@ func (u *user) AddUser(ctx context.Context, user model.User) (*model.User, error
 
 	res, err := u.DB.ExecContext(ctx, query, user.Name, user.Surname, user.Email, user.Password)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error was ocured during executing method ExecContext: %v", err)
 	}
 
 	user.ID, err = res.LastInsertId()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error was ocured during executing method LastInsertId: %v", err)
 	}
 
 	return &user, nil
 }
 
-func (u *user) GetUser() {
+func (u *user) GetUser(ctx context.Context, id int) (*model.User, error) {
+	x := &model.User{}
+
+	query := `select * from users where id=?`
+	row := u.DB.QueryRowContext(ctx, query, id)
+	if err := row.Err(); err != nil {
+		return nil, fmt.Errorf("error was ocured during executing method QueryRowContext: %v", err)
+	}
+
+	err := row.Scan(&x.ID, &x.Name, &x.Surname, &x.Email, &x.Password)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, sql.ErrNoRows
+		} else {
+			return nil, err
+		}
+	}
+	return x, nil
 }
+
+// func (u *user) GetUserforAuth(ctx context.Context, email, password string) (*model.User, error) {
+// }
--- backend/internal/repository/repository_user_test.go
+++ backend/internal/repository/repository_user_test.go
@@ -26,6 +26,10 @@ func TestMain(m *testing.M) {
 }
 
 func Test_user_AddUser(t *testing.T) {
+	_, err := db.Exec("delete from sqlite_sequence;")
+	if err != nil {
+		t.Fail()
+	}
 	type fields struct {
 		DB *sqlx.DB
 	}
@@ -80,8 +84,130 @@ func Test_user_AddUser(t *testing.T) {
 			}
 		})
 	}
-	_, err := db.Exec("delete from users")
+	_, err = db.Exec("delete from users;")
 	if err != nil {
 		t.Fail()
 	}
 }
+
+// func TestNewUserRepo(t *testing.T) {
+// 	_, err := db.Exec("INSERT INTO users (name, surname, email, password) VALUES ('aldik','beka','sadds','asdasd')")
+// 	if err != nil {
+// 		t.Fail()
+// 	}
+// 	type args struct {
+// 		db *sqlx.DB
+// 	}
+// 	tests := []struct {
+// 		name string
+// 		args args
+// 		want *user
+// 	}{
+// 		// TODO: Add test cases.
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			if got := NewUserRepo(tt.args.db); !reflect.DeepEqual(got, tt.want) {
+// 				t.Errorf("NewUserRepo() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
+
+func Test_user_GetUser(t *testing.T) {
+	_, err := db.Exec("INSERT INTO users (name, surname, email, password) VALUES ('aldik','beka','alike','qwqwewe')")
+	if err != nil {
+		t.Fail()
+	}
+	type fields struct {
+		DB *sqlx.DB
+	}
+	type args struct {
+		ctx context.Context
+		id  int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *model.User
+		wantErr bool
+	}{
+		{
+			name: "Beka",
+			fields: fields{
+				DB: db,
+			},
+			args: args{
+				ctx: context.Background(),
+				id:  1,
+			},
+			want: &model.User{
+				ID:       1,
+				Name:     "aldik",
+				Surname:  "beka",
+				Email:    "alike",
+				Password: "qwqwewe",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u := &user{
+				DB: tt.fields.DB,
+			}
+			got, err := u.GetUser(tt.args.ctx, tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("user.GetUser() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("user.GetUser() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+	// _, err = db.Exec("delete from users")
+	// if err != nil {
+	// 	t.Fail()
+	// }
+	// _, err = db.Exec("delete from sqlite_sequence;")
+	// if err != nil {
+	// 	t.Fail()
+	// }
+}
+
+// func Test_user_GetUserforAuth(t *testing.T) {
+// 	type fields struct {
+// 		DB *sqlx.DB
+// 	}
+// 	type args struct {
+// 		ctx      context.Context
+// 		email    string
+// 		password string
+// 	}
+// 	tests := []struct {
+// 		name    string
+// 		fields  fields
+// 		args    args
+// 		want    *model.User
+// 		wantErr bool
+// 	}{
+// 		// TODO: Add test cases.
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			u := &user{
+// 				DB: tt.fields.DB,
+// 			}
+// 			got, err := u.GetUserforAuth(tt.args.ctx, tt.args.email, tt.args.password)
+// 			if (err != nil) != tt.wantErr {
+// 				t.Errorf("user.GetUserforAuth() error = %v, wantErr %v", err, tt.wantErr)
+// 				return
+// 			}
+// 			if !reflect.DeepEqual(got, tt.want) {
+// 				t.Errorf("user.GetUserforAuth() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
