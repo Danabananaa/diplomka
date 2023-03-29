f2cc28da145b5541d49be13c6184c952f3b1b45f
aldiyar.gibadatov@gmail.com <student@ALEM-F4-L04-08.alem.school>
2023-03-29 16:30:14 +0600

    add repo interface

--- backend/internal/model/interface_repository.go
+++ backend/internal/model/interface_repository.go
@@ -1,7 +1,13 @@
 package model
 
-type UserRepo interface {
-	// GetUser()
-}
+type UserRepo interface{}
 
 type SessionRepo interface{}
+
+type SpendingRepo interface{}
+
+type IncomeRepo interface{}
+
+type SpendingTypeRepo interface{}
+
+type IncomeTypeRepo interface{}
