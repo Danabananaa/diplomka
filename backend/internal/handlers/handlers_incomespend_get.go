924b3648108574615e6031339d50c8f8640dc83a
Your Name <you@example.com>
2023-04-05 20:19:23 +0600

    added end and start date

--- backend/internal/handlers/handlers_incomespend_get.go
+++ backend/internal/handlers/handlers_incomespend_get.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 type income_spending struct {
@@ -24,16 +25,32 @@ func (i *income_spending) GetIncomeSpending(w http.ResponseWriter, r *http.Reque
 	w.Header().Set("Content-Type", "application/json")
 	bet := model.Between{}
 
-	err := json.NewDecoder(r.Body).Decode(&bet)
-	if err != nil {
-		log.Printf("json decode: %v", err)
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		return
-	}
+	// err := json.NewDecoder(r.Body).Decode(&bet)
+	// if err != nil {
+	// 	log.Printf("json decode: %v", err)
+	// 	http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+	// 	return
+	// }
 
 	userID := GetID(w, r)
 
 	bet.UserID = userID
+
+	enddate, err := time.Parse("2006-01-02", time.Now().Format("2006-01-02"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	startdate := FindFirstDay(enddate)
+
+	bet.StartDate = model.CustomTime{
+		Time: startdate,
+	}
+
+	bet.EndDate = model.CustomTime{
+		Time: enddate,
+	}
 
 	inc, err := i.IncomeService.GetIncomeService(r.Context(), bet)
 	if err != nil {
@@ -51,11 +68,20 @@ func (i *income_spending) GetIncomeSpending(w http.ResponseWriter, r *http.Reque
 		Income:   inc,
 		Spending: spen,
 	}
-	fmt.Println(spen, inc)
 
 	err = json.NewEncoder(w).Encode(mergerstruct)
 	if err != nil {
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
+}
+
+func FindFirstDay(t time.Time) time.Time {
+	x := t.Month()
+	g := time.Date(int(t.Year()), x, 1, 0, 0, 0, 0, time.UTC)
+	return g
+}
+
+func NewCustomTime(t time.Time) model.CustomTime {
+	return model.CustomTime{t}
 }
