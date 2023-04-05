3ac2adeb4d8cd500c555622bd3fe31692f256d6b
Akhmetzhan Seitkhan <Raikvaza@gmail.com>
2023-04-05 20:34:30 +0600

    Budget page loader

--- client/src/api/loaders.js
+++ client/src/api/loaders.js
@@ -14,41 +14,69 @@ export const homeLoader = () => {
 
 
 export const typesData =( async () => {
-        try {
+  const token = localStorage.getItem('token');
+      
+//   const currentDate = new Date();
+// const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+
+// const formatDate = (date) => {
+//   const year = date.getFullYear();
+//   const month = String(date.getMonth() + 1).padStart(2, '0');
+//   const day = String(date.getDate()).padStart(2, '0');
+  
+//   return `${year}-${month}-${day}`;
+// };
+
+// const firstDayOfMonthFormatted = formatDate(firstDayOfMonth);
+// const currentDateFormatted = formatDate(currentDate);
+  
+  
+  try {
         //   const url = new URL(request.url);
         //   const searchParams = url.searchParams;
         //   const id = searchParams.get('id');
             
-          const [incomeTypeResponse, spendingTypeResponse] = await Promise.all([
-            fetch(`/income/type`, {
-              headers: {
-                Accept: "application/json",    
-            },
-              method: "GET",
-            }),
-            fetch(`/spending/type`, {
+          const [budgetTypeResponse, budgetStatsResponse] = await Promise.all([
+            fetch(`/budget/type`, {
               headers: {
                 Accept: "application/json",
+                Authorization: `${token}`,
               },
               method: "GET",
+              
+            }),
+            fetch(`/budget/stats`, {
+              headers: {
+                Accept: "application/json",
+                Authorization: `${token}`,
+              },
+              method: "GET",
+              
             })
           ]);
             
-          if (!incomeTypeResponse.ok) {
-            const error = new Error(`Could not fetch the post. Status: ${incomeTypeResponse.statusText}`);
-            error.status = incomeTypeResponse.status;
+          if (!budgetTypeResponse.ok) {
+            // if (budgetTypeResponse.status===401){
+            //   return redirect('/signin')
+            // }
+            const error = new Error(`Could not fetch the post. Status: ${budgetTypeResponse.statusText}`);
+            error.status = budgetTypeResponse.status;
             throw error;
           }
           
-          if (!spendingTypeResponse.ok) {
-            const error = new Error(`Could not fetch the comments. Status: ${spendingTypeResponse.statusText}`);
-            error.status = spendingTypeResponse.status;
+          if (!budgetStatsResponse.ok) {
+            // if (budgetStatsResponse.status===401){
+            //   return redirect('/signin')
+
+            // }
+            const error = new Error(`Could not fetch the comments. Status: ${budgetStatsResponse.statusText}`);
+            error.status = budgetStatsResponse.status;
             throw error;
           }
           
-          const incomeTypes = await incomeTypeResponse.json();
-          const spendingTypes = await spendingTypeResponse.json();
-          return { incomeTypes, spendingTypes };
+          const Types = await budgetTypeResponse.json();
+          const Stats = await budgetStatsResponse.json();
+          return { Types, Stats };
         } catch (error) {
           console.log(error);
           throw error;
