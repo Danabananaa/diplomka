a04d47df42328f7a06230fbb3517d614523086b3
aldik <aldiyar.gibadtov@gmail.com>
2023-05-13 01:11:39 +0600

    change query repo planner post

--- backend/internal/repository/repository_planner.go
+++ backend/internal/repository/repository_planner.go
@@ -29,20 +29,27 @@ func (r *repo) AddPlannerSpending(ctx context.Context, planner model.SpendingPla
 }
 
 func (r *repo) GetPlanner(ctx context.Context, bet model.Between) (map[int]model.Planner, error) {
-	query := `
-	SELECT spendingtype.id, COALESCE(planner.amount, 0) as 'amount', COALESCE(spending.amount, 0) as 'spending'
+	// query := `
+	// SELECT spendingtype.id, COALESCE(planner.amount, 0) as 'amount', COALESCE(spending.amount, 0) as 'spending'
+	// FROM spendingtype
+	// LEFT JOIN (
+	//   SELECT *
+	//   FROM spending
+	//   WHERE spending.user_id = :user_id AND spending.date BETWEEN :start_date AND :end_date
+	// ) AS spending ON spending.spending_type = spendingtype.id
+	// LEFT JOIN (
+	//   SELECT *
+	//   FROM planner
+	//   WHERE planner.user_id = :user_id
+	// ) AS planner ON planner.spending_type_id = spendingtype.id
+	// GROUP BY spendingtype.spending_type
+	// ORDER BY spendingtype.id;`
+
+	query := `SELECT spendingtype.id, COALESCE(planner.amount, 0) as 'amount', COALESCE(SUM(spending.amount), 0) as 'spending'
 	FROM spendingtype
-	LEFT JOIN (
-	  SELECT *
-	  FROM spending
-	  WHERE spending.user_id = :user_id AND spending.date BETWEEN :start_date AND :end_date
-	) AS spending ON spending.spending_type = spendingtype.id
-	LEFT JOIN (
-	  SELECT *
-	  FROM planner
-	  WHERE planner.user_id = :user_id
-	) AS planner ON planner.spending_type_id = spendingtype.id
-	GROUP BY spendingtype.spending_type
+	LEFT JOIN planner ON planner.spending_type_id = spendingtype.id AND planner.user_id = :user_id
+	LEFT JOIN spending ON spending.spending_type = spendingtype.id AND spending.user_id = :user_id AND spending.date BETWEEN :start_date AND :end_date
+	GROUP BY spendingtype.id
 	ORDER BY spendingtype.id;`
 
 	rows, err := r.DB.NamedQueryContext(ctx, query, bet)
